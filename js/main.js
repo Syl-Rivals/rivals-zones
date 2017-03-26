e21db2bd4184a398e02decef3dd8dffb75eb3afa
Syl-Rivals <Syl-Rivals@users.noreply.github.com>
2017-03-26 07:31:41 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 var rivals = rivals || {};rivals.utils = rivals.utils || {};
 rivals.jsonTimes = [
 	{ Name: '[R]NetanelG', Timezone: 3 },
-	{ Name: '[R]Bernado', Timezone: -1 },
+	{ Name: '[R]Bernado', Timezone: 0 },
 	{ Name: '[R[Vitold', Timezone: 11 },
 	{ Name: '[R] Syl', Timezone: -3 },
 	{ Name: '[R] Shanks', Timezone: -2 },
 	{ Name: '[R]NastyCloud', Timezone: 1 },
-	{ Name: '[R]Hollie', Timezone: 0 },
+	{ Name: '[R]Hollie', Timezone: -1 },
 	{ Name: '[R]Spartan117', Timezone: -7 },
 	{ Name: '[R] QB300', Timezone: 11 },
 	{ Name: '[R]Jan :)', Timezone: 0 },
