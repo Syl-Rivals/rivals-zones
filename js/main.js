d47192a6f27bdcb9e428859e5ff3acafca03e385
Syl-Rivals <Syl-Rivals@users.noreply.github.com>
2017-05-09 07:47:40 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@ rivals.jsonTimes = [
 	{ Name: '颶風', Timezone: +4.5 },
 	{ Name: 'Guru', Timezone: +3 },
 	{ Name: 'Champagne', Timezone: -2 },
-	{ Name: 'Special', Timezone: -5 },
+	{ Name: 'Special', Timezone: -6 },
 	{ Name: '[R]GR!NGO', Timezone: 10 },
 	{ Name: '[R]Deadline', Timezone: 2 },
 	{ Name: '[R]TomTom', Timezone: 12 },
